dbba3a09c7278f75d0e4dfe71904756cc204c6df
Gui Satoru <guisatoru11@gmail.com>
2026-04-17 17:25:21 -0300

    fix: filter stores with no active headcount employees

--- stores/api/views.py
+++ stores/api/views.py
@@ -1,14 +1,26 @@
+from django.db.models import Count, Q
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from stores.models import Store
 from .serializers import StoreSerializer
 
+
 class StoreListAPIView(APIView):
     def get(self, request):
-        stores = Store.objects.filter(
+        stores = Store.objects.annotate(
+            active_headcount_employees=Count(
+                "employees",
+                filter=Q(
+                    employees__is_active=True,
+                    employees__counts_in_store_headcount=True,
+                ),
+            )
+        ).filter(
             contracted_headcount__isnull=False,
             contracted_headcount__gt=0,
+            active_headcount_employees__gt=0,
         ).order_by("name")
 
         serializer = StoreSerializer(stores, many=True)
